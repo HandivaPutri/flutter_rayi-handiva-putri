772e5d644a2f13d83e960f694b152ae06a72ec70
HandivaPutri <handivaputri15@gmail.com>
2022-09-09 16:17:19 +0700

    ini tugas 2

---
+++ 9_Collection/praktikum/tugas2.dart
@@ -0,0 +1,12 @@
+void main() async {
+  List nama = ['Diva', 'Jojo', 'Miya', 'Snowy'];
+  List umur = [20, 21, 20, 21];
+  print('Nama mahasiswa : ');
+  print(nama);
+  print('Umur mahasiswa : ');
+  print(umur);
+
+  print('Daftar Mahasiswa : ');
+  Map<String, dynamic> data = {'nama': 'Diva', 'umur': 20};
+  print(data);
+}
