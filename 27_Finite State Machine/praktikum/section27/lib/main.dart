a8329e7e4c003d0c092e583988c77031a88b2e18
HandivaPutri/flutter_rayi-handiva-putri <https://github.com/HandivaPutri/flutter_rayi-handiva-putri.git>
2022-10-17 16:33:10 +0700

    Finite State Machine section 27

--- 27_Finite State Machine/praktikum/section27/lib/main.dart
+++ 27_Finite State Machine/praktikum/section27/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:section27/screen/contact/food_screen.dart';
-import 'package:section27/screen/contact/food_view_model.dart';
+import 'package:section27/screen/food/food_screen.dart';
+import 'package:section27/screen/food/food_view_model.dart';
 
 void main() {
   runApp(
